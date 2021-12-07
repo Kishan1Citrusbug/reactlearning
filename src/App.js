56b6f323d72eba088cf50fbb3816a2f432f85e50
Kishan Patel <kishanpatelofficial@gmail.com>
2021-12-07 14:06:10 +0530

    timer with timeout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './TicTacToe.css';
-import Game from './Game.js';
+import React,{useState,useEffect} from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          <Game></Game>
-        </p>
-      </div>
-    );
-  }
+
+export default function App() {
+  const [isActive, setIsActive] = useState(false);
+  const [isStopped, setIsStopped] = useState(true);
+  const [time, setTime] = useState(0);
+  
+  useEffect(() => {
+      let interval = null;
+    if (isActive && isStopped === false) {
+      interval = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, isStopped]);
+  
+  useEffect(() => {
+    console.log("timeout called")
+    const timeout = setTimeout(() => {
+      setIsActive(!isActive)
+      setTime(0);
+    }, 5000);
+    return () => {
+      clearTimeout(timeout)
+    }
+  },[isActive])
+  const handleStart = () => {
+    setIsActive(true);
+    setIsStopped(false);
+  };
+  
+  const handlePauseResume = () => {
+    setIsStopped(!isStopped);
+  };
+  
+  return <div><h1 > {time} </h1>
+    <button onClick={handleStart} >start</button>  
+    <button onClick={handlePauseResume} >stop</button>  
+    </div>
 }
-
-export default App;
