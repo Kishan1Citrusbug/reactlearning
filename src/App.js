f55af72a9305308204ce9a3152acd5a3d371084a
Kishan Patel <kishanpatelofficial@gmail.com>
2021-12-15 13:01:43 +0530

    usecontext

--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
-import React, { useReducer, useState } from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+import SwitchButton from "./Button";
+import "./App.css";
 
-
-export default function App() {
-  const [desc, setDesc] = useState('')
-  const itemReducer = (items, action) => {
-    switch (action.type) {
-      case "Add desc":
-        return [...items, newItem(action.payload.desc)]
-      case "delete desc":
-        return items.filter(item => item.id !== action.payload.id)
-      default:
-        return items
-    }
-  }
-  const newItem = (desc) => {
-    return {
-      id: Date.now(),
-      desc: desc
-    }
-  }
-  const [items, dispatch] = useReducer(itemReducer, []);
-
-  const addBlock = (e) => {
-    e.preventDefault()
-    dispatch({
-      type: "Add desc",
-      payload: {
-        desc: desc
-      }
-    })
-    setDesc("")
-  }
-  console.log(items)
+function App() {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div>
-      <form>
-        <label>
-          description:
-          <input type="text"
-            value={desc}
-            onChange={(e) => setDesc(e.target.value)}
-          />
-        </label>
-        <button onClick={(e) => addBlock(e)}>add</button>
-      </form>
-      {items.map(item => {
-        return (
-          <div>
-            <h6>id: {item.id}</h6>
-            <h1>Desc: {item.desc}</h1>
-            <button onClick={() => dispatch({ type: "delete desc", payload: { id: item.id } })}>Delete</button>
-          </div>)
-      })}
+    <div className={`bg ${darkMode ? "bg-dark" : "bg-light"}`}>
+      <h1 className={`heading ${darkMode ? "heading-dark" : "heading-light"}`}>
+        {darkMode ? "Dark Mode" : "Light Mode"}
+      </h1>
+      <p className={`para ${darkMode ? "para-dark" : "para-light"}`}>
+       acbdcdbcdf
+      </p>
+      <SwitchButton />
     </div>
-  )
+  );
 }
+
+export default App;
